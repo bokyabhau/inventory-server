cafb91d5ec348f0fe8bdc4f12b02a479a9fd924e
BokyaBhau <shivambhuadi@gmail.com>
2023-09-24 12:32:00 +0200

    update port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ROUTE_PREFIX} from './config';
+import { ROUTE_PREFIX } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix(ROUTE_PREFIX);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
